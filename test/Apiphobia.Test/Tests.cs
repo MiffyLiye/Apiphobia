60362eb8679b6b05e35b8c7e9b4da158a35eb703
MiffyLiye <miffyliye@gmail.com>
2016-08-29 12:27:22 +0800

    Create WebApi and Test projects

---
+++ test/Apiphobia.Test/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+
+namespace Apiphobia.Test
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
